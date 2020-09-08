bb92f997be87bebd62616dff93b78dd1ec47111e
Audrey Cooper <audrey_cooper@me.com>
2020-09-08 14:24:14 -0700

    counter funcitonality implemented in js file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-  // --- our code goes here ---
+
+  const $tweet = $('#tweet-text');
+  
+  $tweet.keyup(function() {
+    console.log ('tweet.change', this)
+    const txtLength = $("#tweet-text").val().length;
+    console.log(txtLength); 
+    // const $counter = $('output.counter');
+    $("output.counter").html(function () {
+      return 140 - txtLength;
+    });
+  });
+
 });
