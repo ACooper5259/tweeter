0d0a2608525cab429453dc0d018e9ff589d446c1
Audrey Cooper <audrey_cooper@me.com>
2020-09-09 16:17:13 -0700

    Hard cded tweets deleted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const tweets = require("../../server/routes/tweets");
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 $(() => {
   const renderTweets = function (tweets) {
     for (const tweet of tweets) {
@@ -58,18 +32,13 @@ $(() => {
     return $tweet;
   }
 
-  renderTweets(data);
 
   // POST request for new tweets 
   const $tweetForm = $('#new-tweet-form');
   $tweetForm.on('submit', function (event) {
-    // prevent default behaviour
     event.preventDefault();
-    // serialize data
     const serializedTweet = $(this).serialize();
-    // submit data to POST route
     $.post('/tweets', serializedTweet)
-    // console.log("request sent")
   });
 
   // GET request for tweets to /tweets
